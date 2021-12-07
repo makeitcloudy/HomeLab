755bc1ba26ab740c41de7f7d37daec7f366d5c93
tansei <tansei@o2.pl>
2021-12-07 21:08:40 +0100

    Create readme.ps1

---
+++ 009/readme.ps1
@@ -0,0 +1,8 @@
+#RDS, RDMi , WVD (Windows Virtual Desktop), AVD (Azure Virtual Desktop)
+
+Start-Process "https://docs.microsoft.com/en-us/sysinternals/downloads/rdcman" #Remote Desktop Connection Manager from SysInternals
+
+Start-Process "https://docs.microsoft.com/en-us/sysinternals/" #Sysinternals suite
+Start-Process "https://devolutions.net/"
+Start-Process "https://blog.devolutions.net/"
+Start-Process "https://www.youtube.com/watch?v=Y4-ZFzC9GKE"
