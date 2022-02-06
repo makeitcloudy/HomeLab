a2d084df5e78ca44421055fa47ddef57762fad7f
tansei <tansei@o2.pl>
2022-02-06 18:07:11 +0100

    Create readme-storage.ps1

---
+++ 002_storage/readme-storage.ps1
@@ -0,0 +1,3 @@
+#NFS and samba on centos
+Start-Process 'https://www.linuxtechi.com/setup-nfs-server-on-centos-8-rhel-8/'
+Start-Process 'https://www.linuxbabe.com/redhat/set-up-samba-server-on-centos-8-rhel-8-for-file-sharing'
