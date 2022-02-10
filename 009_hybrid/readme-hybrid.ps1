2f1a17462d35c5e46ea5d1fd07ad8160663c6c09
tansei <tansei@o2.pl>
2022-02-10 08:36:10 +0100

    Update readme-hybrid.ps1

--- 009_hybrid/readme-hybrid.ps1
+++ 009_hybrid/readme-hybrid.ps1
@@ -1,8 +1,11 @@
 #RDS, RDMi , WVD (Windows Virtual Desktop), AVD (Azure Virtual Desktop)
 
-Start-Process "https://docs.microsoft.com/en-us/sysinternals/downloads/rdcman" #Remote Desktop Connection Manager from SysInternals
+Start-Process 'https://docs.microsoft.com/en-us/sysinternals/downloads/rdcman' #Remote Desktop Connection Manager from SysInternals
 
-Start-Process "https://docs.microsoft.com/en-us/sysinternals/" #Sysinternals suite
-Start-Process "https://devolutions.net/"
-Start-Process "https://blog.devolutions.net/"
-Start-Process "https://www.youtube.com/watch?v=Y4-ZFzC9GKE"
+Start-Process 'https://docs.microsoft.com/en-us/sysinternals/' #Sysinternals suite
+Start-Process 'https://devolutions.net/'
+Start-Process 'https://blog.devolutions.net/'
+Start-Process 'https://www.youtube.com/watch?v=Y4-ZFzC9GKE'
+
+
+Start-Process 'https://www.citrix.com/downloads/citrix-cloud/product-software/automated-configuration.html'
