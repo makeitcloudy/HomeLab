2c2f9c4478228f96eef266c869246c9248396224
tansei <tansei@o2.pl>
2022-03-26 19:02:35 +0100

    Update readme-url-dsc.ps1

--- 008_DSC/readme-url-dsc.ps1
+++ 008_DSC/readme-url-dsc.ps1
@@ -5,3 +5,6 @@ Start-Process 'https://dscottraynsford.wordpress.com/'
 Start-Process 'https://plagueho.github.io/'
 
 Start-Process 'https://gitter.im/PlagueHO/LabBuilder?at=57437c8afce033da4bef4936'
+
+Start-Process 'https://github.com/PlagueHO/LabBuilder/blob/main/source/samples/Sample_WS2016_DomainComplete.xml'
+Start-Process 'https://github.com/PlagueHO/LabBuilder/blob/main/source/dsclibrary/STANDALONE_ROOTCA.DSC.ps1'
