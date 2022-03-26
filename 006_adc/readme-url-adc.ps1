eacd9cb6cb56b189cdca5c0e12ef3d4eff1f476a
tansei <tansei@o2.pl>
2022-03-26 18:36:24 +0100

    Update and rename readme-adc.ps1 to readme-url-adc.ps1

--- 006_adc/readme-url-adc.ps1
+++ 006_adc/readme-url-adc.ps1
@@ -3,4 +3,6 @@ Start-Process 'https://twitter.com/johnbillekens/
 Start-Process 'https://www.citrix.com/blogs/2019/06/24/why-certificates-are-more-important-today-than-ever/' #
 Start-Process 'https://github.com/j81blog/GenLeCertForNS/releases/tag/v2.6.3' #Let's encrypt cert for ns
 Start-Process 'https://www.citrix.com/blogs/author/santiagoca/' #NS REST-API
+
+Start-Process 'https://github.com/santiagocardenas/GeoLocationAPI' #MaxMind Geo API
 #endregion
