97fd9aa27dd8250d850afc4b6a3df75be07f3c32
tansei <tansei@o2.pl>
2022-03-26 17:51:39 +0100

    Create readme-Url.ps1

---
+++ 001_hypervisor/readme/readme-Url.ps1
@@ -0,0 +1,23 @@
+https://github.com/xapi-project/xen-api-sdk/tree/master/powershell
+https://github.com/xapi-project/xen-api-sdk/blob/master/powershell/autogen/samples/AutomatedTestCore.ps1
+
+Start-Process "https://workspace-guru.com/2017/11/12/scripting-citrix-xenserver-powershell-command-line/"
+Start-Process "https://discussions.citrix.com/topic/414777-xenserver-boot-mode-uefi-boot-for-machines-created-with-powershell-api/" #thread how to create UEFI boot vm
+Start-Process "https://xcp-ng.org/forum/topic/5180/xen-boot-mode-uefi-boot-for-machines-created-with-powershell-api" #thread how to create UEFI boot vm
+
+Start-Process 'http://bisbd.blogspot.com/2020/09/modifying-citrix-hypervisor-xenserver.html' #HVM-boot-params
+Start-Process 'https://docs.citrix.com/en-us/citrix-hypervisor/vms/windows.html#guest-uefi-boot-and-secure-boot'
+Start-Process 'https://xcp-ng.org/docs/cli_reference.html#low-level-list-commands'
+
+Start-Process 'https://github.com/ZachThurmond/Automated-XenServer-Labs/blob/master/AXL.ps1'
+Start-Process 'https://github.com/xapi-project/xen-api-sdk/blob/master/powershell/autogen/samples/AutomatedTestCore.ps1'
+
+Start-Process 'https://docs.citrix.com/en-us/citrix-hypervisor/vms/windows.html' #UEFI machines on XenServer
+
+#XS NFS shares
+Start-Process 'https://computingforgeeks.com/add-and-use-iso-library-storage-repository-xen-xcp-ng/'
+
+#CPU topology
+Start-Process 'https://docs.citrix.com/en-us/xencenter/7-1/vms-new-cpu-memory.html'
+Start-Process 'https://discussions.citrix.com/topic/379901-socket-with-cores-or-sockets-with-core/'
+Start-Process 'https://support.citrix.com/article/CTX126524'
