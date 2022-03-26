cbe33be3914c5b29af5f06bd9946c2c81b2557be
tansei <tansei@o2.pl>
2022-03-26 18:38:03 +0100

    Update and rename readme-imagePrep.ps1 to readme-url-imagePrep.ps1

--- 007_imagePrep/readme-imagePrep.ps1
+++
@@ -1,1 +0,0 @@
-#unattended xml files for the LTSC iso's which can be downloaded from the microsoft webpage
---
+++ 007_imagePrep/readme-url-imagePrep.ps1
@@ -0,0 +1,2 @@
+#unattended xml files for the LTSC iso's which can be downloaded from the microsoft webpage
+Start-Process 'https://serverfault.com/questions/979174/how-can-i-do-an-offline-update-of-a-new-windows-2012-r2-server' #offline servicing Windows Server 2012R2
