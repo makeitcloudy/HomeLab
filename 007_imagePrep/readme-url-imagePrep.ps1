fc5712b79a79b92d79e25fc4a44535f342d7a965
tansei <tansei@o2.pl>
2022-03-26 18:56:17 +0100

    Update readme-url-imagePrep.ps1

--- 007_imagePrep/readme-url-imagePrep.ps1
+++ 007_imagePrep/readme-url-imagePrep.ps1
@@ -1,3 +1,4 @@
 #unattended xml files for the LTSC iso's which can be downloaded from the microsoft webpage
 Start-Process 'https://github.com/ibeerens/osdbuilder' #OSDBuilder howto
+Start-Process 'https://github.com/pbatard/rufus/issues/1077' #Rufus - Increase time showing "Press any key to boot from USB" to e.g. 30 seconds
 Start-Process 'https://serverfault.com/questions/979174/how-can-i-do-an-offline-update-of-a-new-windows-2012-r2-server' #offline servicing Windows Server 2012R2
