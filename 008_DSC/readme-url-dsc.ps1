e13f009ed722b8760b468b6b146f117472da46e8
tansei <tansei@o2.pl>
2022-03-26 19:01:09 +0100

    Create readme-url-dsc.ps1

---
+++ 008_DSC/readme-url-dsc.ps1
@@ -0,0 +1,7 @@
+Start-Process 'https://github.com/PlagueHO/LabBuilder/tree/main/source/dsclibrary' #DSC code for various Windows Roles
+Start-Process 'https://dscottraynsford.wordpress.com/category/dsc/' #Install Windows Admin Center using DSC
+Start-Process 'https://dscottraynsford.wordpress.com/category/certificate-services/'
+Start-Process 'https://dscottraynsford.wordpress.com/'
+Start-Process 'https://plagueho.github.io/'
+
+Start-Process 'https://gitter.im/PlagueHO/LabBuilder?at=57437c8afce033da4bef4936'
