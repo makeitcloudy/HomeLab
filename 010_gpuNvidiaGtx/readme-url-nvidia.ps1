1f89d8d37ac23fa43d26b3d4449fd0a5edcecaa0
tansei <tansei@o2.pl>
2022-03-26 19:07:08 +0100

    Create readme-url-nvidia.ps1

---
+++ 010_gpuNvidiaGtx/readme-url-nvidia.ps1
@@ -0,0 +1,17 @@
+#**GPU Virtualization**
+
+Start-Process 'https://williamlam.com/2020/06/passthrough-of-integrated-gpu-igpu-for-standard-intel-nuc.html'
+Start-Process 'https://www.nvidia.com/Download/index.aspx'
+Start-Process 'https://developer.nvidia.com/cuda/wsl/download'
+
+#allows using their video cards in the Linux "super VM" while using a Windows host OS and what OP posted here
+#allows using their video cards in Windows while using a Linux host OS.
+Start-Process 'https://www.storagereview.com/news/nvidia-enables-beta-support-for-virtualization-on-geforce-gpus'
+Start-Process 'https://www.reddit.com/r/VFIO/comments/mnp8ze/vgpu_unlock_unlock_vgpu_functionality_for/'
+Start-Process 'https://gist.github.com/HiFiPhile/b3267ce1e93f15642ce3943db6e60776'
+Start-Process 'https://github.com/DualCoder/vgpu_unlock'
+Start-Process 'https://docs.google.com/document/d/1pzrWJ9h-zANCtyqRgS7Vzla0Y8Ea2-5z2HEi4X75d2Q/edit' #gpu unlock wiki
+Start-Process 'https://looking-glass.io/wiki/Installation_on_other_distributions'
+Start-Process 'https://bestofcpp.com/repo/DualCoder-vgpu_unlock'
+
+Start-Process 'https://www.youtube.com/watch?v=wEhvQEyiOwI'
