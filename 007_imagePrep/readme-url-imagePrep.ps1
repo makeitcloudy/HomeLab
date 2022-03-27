a34df2f0f66cbbe34b3048967ca67188d4d52ea1
tansei <tansei@o2.pl>
2022-03-27 21:17:56 +0200

    Update readme-url-imagePrep.ps1

--- 007_imagePrep/readme-url-imagePrep.ps1
+++ 007_imagePrep/readme-url-imagePrep.ps1
@@ -2,3 +2,5 @@
 Start-Process 'https://github.com/ibeerens/osdbuilder' #OSDBuilder howto
 Start-Process 'https://github.com/pbatard/rufus/issues/1077' #Rufus - Increase time showing "Press any key to boot from USB" to e.g. 30 seconds
 Start-Process 'https://serverfault.com/questions/979174/how-can-i-do-an-offline-update-of-a-new-windows-2012-r2-server' #offline servicing Windows Server 2012R2
+
+Start-Process 'https://github.com/larytet/auto-win/tree/master/autounattend' #unattended xml bios and uefi for windows 10
