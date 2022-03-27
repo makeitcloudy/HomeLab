3f296caaffa1c1a9d6d5767294df71021bba3d2f
tansei <tansei@o2.pl>
2022-03-27 10:25:48 +0200

    Update and rename readme-storage.ps1 to readme-url-storage.ps1

--- 002_storage/readme-storage.ps1
+++
@@ -1,3 +0,0 @@
-#NFS and samba on centos
-Start-Process 'https://www.linuxtechi.com/setup-nfs-server-on-centos-8-rhel-8/'
-Start-Process 'https://www.linuxbabe.com/redhat/set-up-samba-server-on-centos-8-rhel-8-for-file-sharing'
---
+++ 002_storage/readme-url-storage.ps1
@@ -0,0 +1,11 @@
+#Linux Disk Management
+Start-Process 'https://www.youtube.com/watch?v=HsRRX05f1L4' #nerdOnTheStreet #Disk Management - Bash Basics (Linux CLI)
+#LVM
+Start-Process 'https://www.youtube.com/watch?v=krerZ2TZvL8' #nerdOnTheStreet #Intro into LVM
+Start-Process 'https://www.youtube.com/watch?v=MeltFN-bXrQ' #LearnLinuxTV #LVM Deep Dive
+
+#NFS and samba on centos
+Start-Process 'https://www.howtoforge.com/nfs-server-and-client-on-centos-7'
+
+Start-Process 'https://www.linuxtechi.com/setup-nfs-server-on-centos-8-rhel-8/'
+Start-Process 'https://www.linuxbabe.com/redhat/set-up-samba-server-on-centos-8-rhel-8-for-file-sharing'
