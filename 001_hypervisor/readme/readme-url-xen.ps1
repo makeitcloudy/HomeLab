e27991852c83b7da6f8b98f968c69012cf5e7d00
tansei <tansei@o2.pl>
2022-03-27 10:28:58 +0200

    Update readme-url-xen.ps1

--- 001_hypervisor/readme/readme-url-xen.ps1
+++ 001_hypervisor/readme/readme-url-xen.ps1
@@ -1,5 +1,7 @@
-https://github.com/xapi-project/xen-api-sdk/tree/master/powershell
-https://github.com/xapi-project/xen-api-sdk/blob/master/powershell/autogen/samples/AutomatedTestCore.ps1
+Start-Process 'https://github.com/xapi-project/xen-api-sdk/tree/master/powershell'
+Start-Process 'https://github.com/xapi-project/xen-api-sdk/blob/master/powershell/autogen/samples/AutomatedTestCore.ps1'
+
+Start-Process 'https://docs.citrix.com/en-us/citrix-hypervisor/command-line-interface.html' #Xen command line interface
 
 Start-Process "https://workspace-guru.com/2017/11/12/scripting-citrix-xenserver-powershell-command-line/"
 Start-Process "https://discussions.citrix.com/topic/414777-xenserver-boot-mode-uefi-boot-for-machines-created-with-powershell-api/" #thread how to create UEFI boot vm
