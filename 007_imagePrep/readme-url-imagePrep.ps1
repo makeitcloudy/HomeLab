92388b1012d8a074e5a1f0179bc7b284610ed406
makeitcloudy <tansei@o2.pl>
2022-04-06 20:56:45 +0200

    Update readme-url-imagePrep.ps1

--- 007_imagePrep/readme-url-imagePrep.ps1
+++ 007_imagePrep/readme-url-imagePrep.ps1
@@ -3,5 +3,7 @@ Start-Process 'https://github.com/ibeerens/osdbuilder' #OSDBuilder howto
 Start-Process 'https://github.com/pbatard/rufus/issues/1077' #Rufus - Increase time showing "Press any key to boot from USB" to e.g. 30 seconds
 Start-Process 'https://serverfault.com/questions/979174/how-can-i-do-an-offline-update-of-a-new-windows-2012-r2-server' #offline servicing Windows Server 2012R2
 
+Start-Process 'https://citrixguyblog.com/2019/03/19/osdbuilder-reference-image-on-steroids/'
+
 Start-Process 'https://github.com/larytet/auto-win/tree/master/autounattend' #unattended xml bios and uefi for windows 10
 Start-Process 'https://github.com/Mohrpheus78/Citrix/tree/main/PVS%20Admin%20Toolkit' #PVS Admin Toolkit
