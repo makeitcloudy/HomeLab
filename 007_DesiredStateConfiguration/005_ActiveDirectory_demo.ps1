8161ed73feeab6540a9ca64044d4da4bd4ea489d
Piotr Ostrowski <tansei@o2.pl>
2024-07-03 14:58:57 +0200

    2024.07.03

---
+++ 007_DesiredStateConfiguration/005_ActiveDirectory_demo.ps1
@@ -0,0 +1,15 @@
+$githubUrl            = 'https://raw.githubusercontent.com/makeitcloudy/AutomatedLab/feature/007_DesiredStateConfiguration/005_ActiveDirectory'
+$fileName             = 'ADDS_setup.ps1'
+$uri                  = $githubUrl,$fileName -join '/'
+
+$outDirectoryPath     = "$env:USERPROFILE\Documents"
+$outDirectoryFullPath = Join-Path $outDirectoryPath -ChildPath $fileName
+
+
+
+Invoke-WebRequest -Uri $uri -OutFile $outDirectoryFullPath
+#psedit $outDirectoryFullPath
+
+if(Test-Path -Path $outDirectoryFullPath){
+    & $outDirectoryFullPath -Computername 'dc01'
+}
